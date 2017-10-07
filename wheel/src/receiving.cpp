f481f5f3255c24c6ca7f0d8daadead1ce8f8bbf1
Joel Newman <joellnewman@hotmail.com>
2017-10-07 00:21:19 -0700

    Fixed the issue of global scope parameters
    also added more comments for clarity

--- wheel/src/receiving.cpp
+++ wheel/src/receiving.cpp
@@ -2,6 +2,14 @@
 #include "geometry_msgs/Accel.h"
 #include "geometry_msgs/Vector3.h"
 
+/*
+ * This file simply echos out some of the data that is pushed from
+ * the sending portion of the wheel. This is pretty much just a copy
+ * of the basic pub/sub tutorials.
+ * 
+ * @see http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28c%2B%2B%29
+ * 
+ */ 
 
 void wheelCallback(const geometry_msgs::AccelConstPtr& msg)
 {
@@ -11,7 +19,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "wheellistener");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("motor_1",1000,wheelCallback);
+	ros::Subscriber sub = n.subscribe("motor_12",1000,wheelCallback);
 	ROS_INFO("SUBSCRIBED TO WHEEL_1");
 	ros::spin();
 	return 0;
