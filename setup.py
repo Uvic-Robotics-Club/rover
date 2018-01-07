29f181a9fe3e14c3089ea8ce91350ec31534ce0f
Joel Newman <joellnewman@hotmail.com>
2018-01-06 17:01:31 -0800

    asdf

--- setup.py
+++ setup.py
@@ -5,3 +5,13 @@ Created on Sat Jan 06 16:30:25 2018
 @author: joell
 """
 
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['rover'],
+    package_dir={'': 'src'},
+)
+
+setup(**setup_args)
