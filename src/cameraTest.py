e54906a0570fb15877dd56c15e956cd7c7fb4384
Joel Newman <joellnewman@hotmail.com>
2018-01-19 19:35:23 -0800

    asdf

--- src/cameraTest.py
+++ src/cameraTest.py
@@ -91,22 +91,21 @@ def sendStuff():
                 frame = get_image()
                 cimg = cv2.medianBlur(frame,5)
                 cimg = cv2.cvtColor(cimg,cv2.COLOR_BGR2GRAY)
-                edges = cv2.Canny(cimg,50,150,apertureSize = 3)
+                edges = cv2.Canny(cimg,50,100,apertureSize = 3)
 
                 lines = cv2.HoughLines(edges,1,np.pi/180,200)
                 if(lines == None):
-                    print "LINES IS WHAT THE ISSUE IS"
-                for rho, theta in lines[0]:
-                    a = np.cos(theta)
-                    b = np.sin(theta)
-                    x0 = a*rho
-                    y0 = b*rho
-                    x1 = int(x0 + 1000*(-b))
-                    y1 = int(y0 + 1000*(a))
-                    x2 = int(x0 - 1000*(-b))
-                    y2 = int(y0 - 1000*(a))
+                    for rho, theta in lines[0]:
+                        a = np.cos(theta)
+                        b = np.sin(theta)
+                        x0 = a*rho
+                        y0 = b*rho
+                        x1 = int(x0 + 1000*(-b))
+                        y1 = int(y0 + 1000*(a))
+                        x2 = int(x0 - 1000*(-b))
+                        y2 = int(y0 - 1000*(a))
 
-                    cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                        cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
 
                 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),camera_quality]
                 result, imgencode = cv2.imencode('.jpg', frame, encode_param)
