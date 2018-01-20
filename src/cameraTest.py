cb05814cf58bde4bc18e8b362ba356ef76e15fd2
Joel Newman <joellnewman@hotmail.com>
2018-01-19 19:43:30 -0800

    trying autocanny

--- src/cameraTest.py
+++ src/cameraTest.py
@@ -65,6 +65,18 @@ def readStuff():
         s.close()
     return
 
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+
 def sendStuff():
     global totalExit, HOST
 
@@ -91,21 +103,7 @@ def sendStuff():
                 frame = get_image()
                 cimg = cv2.medianBlur(frame,5)
                 cimg = cv2.cvtColor(cimg,cv2.COLOR_BGR2GRAY)
-                edges = cv2.Canny(cimg,50,100,apertureSize = 3)
-
-                lines = cv2.HoughLines(edges,1,np.pi/180,200)
-                if(lines == None):
-                    for rho, theta in lines[0]:
-                        a = np.cos(theta)
-                        b = np.sin(theta)
-                        x0 = a*rho
-                        y0 = b*rho
-                        x1 = int(x0 + 1000*(-b))
-                        y1 = int(y0 + 1000*(a))
-                        x2 = int(x0 - 1000*(-b))
-                        y2 = int(y0 - 1000*(a))
-
-                        cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                frame = auto_canny(cimg)
 
                 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),camera_quality]
                 result, imgencode = cv2.imencode('.jpg', frame, encode_param)
