ee0d88f5f346bc951f519e3f40bc1dc6e77fed50
Joel Newman <joellnewman@hotmail.com>
2018-01-19 19:29:10 -0800

    asdf

--- src/cameraTest.py
+++ src/cameraTest.py
@@ -91,7 +91,7 @@ def sendStuff():
                 frame = get_image()
                 cimg = cv2.medianBlur(frame,5)
                 cimg = cv2.cvtColor(cimg,cv2.COLOR_BGR2GRAY)
-                edges = cv2.Canny(gray,50,150,apertureSize = 3)
+                edges = cv2.Canny(cimg,50,150,apertureSize = 3)
 
                 lines = cv2.HoughLines(edges,1,np.pi/180,200)
                 for rho, theta in lines[0]:
@@ -104,10 +104,10 @@ def sendStuff():
                     x2 = int(x0 - 1000*(-b))
                     y2 = int(y0 - 1000*(a))
 
-                    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+                    cv2.line(cimg,(x1,y1),(x2,y2),(0,0,255),2)
 
                 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),camera_quality]
-                result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+                result, imgencode = cv2.imencode('.jpg', cimg, encode_param)
 
                 data = np.array(imgencode)
                 stringData = data.tostring()
