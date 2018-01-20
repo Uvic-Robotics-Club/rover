65221c5b7adb4325a9b71829351adb40e2e35432
Joel Newman <joellnewman@hotmail.com>
2018-01-19 19:26:44 -0800

    circles turned into lines

--- src/cameraTest.py
+++ src/cameraTest.py
@@ -90,13 +90,21 @@ def sendStuff():
                 s.send("new frame")
                 frame = get_image()
                 cimg = cv2.medianBlur(frame,5)
-                #cimg = cv2.cvtColor(cimg,cv2.COLOR_GRAY2BGR)
-                circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-                for i in circles[0,:]:
-                    # draw the outer circle
-                    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-                    # draw the center of the circle
-                    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+                cimg = cv2.cvtColor(cimg,cv2.COLOR_BGR2GRAY)
+                edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+                lines = cv2.HoughLines(edges,1,np.pi/180,200)
+                for rho, theta in lines[0]:
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*rho
+                    y0 = b*rho
+                    x1 = int(x0 + 1000*(-b))
+                    y1 = int(y0 + 1000*(a))
+                    x2 = int(x0 - 1000*(-b))
+                    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
                 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),camera_quality]
                 result, imgencode = cv2.imencode('.jpg', frame, encode_param)
