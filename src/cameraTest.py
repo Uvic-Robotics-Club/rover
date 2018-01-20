c3749e1846747f2eb89a39cc1ee366c35ae65e8e
Joel Newman <joellnewman@hotmail.com>
2018-01-19 19:16:04 -0800

    trying to draw circles as well

--- src/cameraTest.py
+++ src/cameraTest.py
@@ -89,6 +89,15 @@ def sendStuff():
                 #dont change the "new frame" this is a keyword for me
                 s.send("new frame")
                 frame = get_image()
+                cimg = cv2.medianBlur(frame,5)
+                cimg = cv2.cvtColor(cimg,cv2.COLOR_GRAY2BGR)
+                circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+                for i in circles[0,:]:
+                    # draw the outer circle
+                    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+                    # draw the center of the circle
+                    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
                 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),camera_quality]
                 result, imgencode = cv2.imencode('.jpg', frame, encode_param)
 
